16a17845223839e0e85e6976383a97d402a957c3
Fadil Anwar <fadilanwar727@gmail.com>
2026-04-04 14:13:52 +0400

    Fix CORS error for dynamic Vercel URLs

--- backend/app/main.py
+++ backend/app/main.py
@@ -24,6 +24,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[settings.FRONTEND_URL, "http://localhost:3000"],
+    allow_origin_regex="https://.*",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
